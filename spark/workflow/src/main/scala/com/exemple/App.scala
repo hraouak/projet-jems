74f9f6011205c5ba2b536acf1b85b7939407b221
Haroun RAOUAK <hraouak@outlook.com>
2024-01-22 14:38:00 +0100

    Add Maven configuration files

--- spark/workflow/src/main/scala/com/exemple/App.scala
+++ spark/workflow/src/main/scala/com/exemple/App.scala
@@ -1,15 +1,20 @@
-package com.exemple
+package com.example.script
 
-/**
- * @author ${user.name}
- */
+import org.apache.spark.sql.SparkSession
+import java.io._
+
+
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
-  }
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder.appName("Spark test").config("spark.sql.warehouse.dir", "/user/mapr/hive/warehouse").enableHiveSupport().getOrCreate()
 
+    val df = spark.table("museums")
+    val lineCount = df.count()
+    val file = new File("/tmp/result.txt")
+    val writer = new PrintWriter(file)
+    writer.write(lineCount.toString)
+    writer.close()
+
+    spark.stop()
+  }
 }
