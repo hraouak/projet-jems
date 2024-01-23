f4aef9314b75b67ba97ec25a39bc0514f22e69ab
Haroun RAOUAK <hraouak@outlook.com>
2024-01-23 15:35:15 +0100

    Update

--- start_stop.sh
+++ start_stop.sh
@@ -3,28 +3,41 @@
 hostname="maprdemo"
 
 while true; do
-    choice=$(dialog --clear --backtitle "Interactive Menu" --title "Select Action" \
+    choice=$(dialog --clear --backtitle "Menu" --title "Select Action" \
                     --menu "Choose an action:" 15 40 4 \
-                    1 "Start History Server" \
-                    2 "Stop History Server" \
-                    3 "Exit" 3>&1 1>&2 2>&3)
+                    1 "Start Job History Server" \
+                    2 "Stop Job History Server" \
+                    3 "Start Spark History Server" \
+                    4 "Stop Spark History Server "\
+                    5 "Exit" 3>&1 1>&2 2>&3)
 
     case $choice in
         1)
             maprcli node services -name historyserver -action start -nodes $hostname
-            maprcli node services -name spark-historyserver -action start -nodes $hostname
-            dialog --infobox "History Server started." 5 30
+            dialog --infobox "Job History Server started." 5 30
             sleep 2
             ;;
         2)
             maprcli node services -name historyserver -action stop -nodes $hostname
-            maprcli node services -name spark-historyserver -action stop -nodes $hostname
-            dialog --infobox "History Server stopped." 5 30
+            dialog --infobox "Job History Server stopped." 5 30
             sleep 2
             ;;
+
         3)
+            maprcli node services -name spark-historyserver -action start -nodes $hostname
+            dialog --infobox "Spark History Server started." 5 30
+            sleep 2
+            ;;        
+
+        4)
+            maprcli node services -name spark-historyserver -action stop -nodes $hostname
+            dialog --infobox "Spark History Server stopped." 5 30
+            sleep 2
+            ;; 
+        5)
             echo "Exiting..."
             exit 0
             ;;
     esac
 done
+clear
