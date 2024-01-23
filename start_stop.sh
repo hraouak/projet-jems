e5a562590524c65da3164aca280845be57c2448a
Haroun RAOUAK <hraouak@outlook.com>
2024-01-23 15:01:40 +0100

    Update

--- start_stop.sh
+++ start_stop.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #path="/opt/mapr/spark/spark-2.3.1/sbin"
-path="/opt/mapr/hadoop/hadoop-2.7.0/sbin"
+#path="/opt/mapr/hadoop/hadoop-2.7.0/sbin"
 hostname="maprdemo"
 
-
+echo 'list of services : \n'
+maprcli service list | awk '{$2, $5}' | column -t
+echo '\n'
 
 while true; do
 echo "If you want to start the History Server, type 'start'."
@@ -18,6 +20,9 @@ if [ "$choice" == "start" ]; then
     #bash $path/mr-jobhistory-daemon.sh start historyserver
     #bash $path/yarn-daemon.sh start historyserver
     maprcli node services -name historyserver -action start -nodes $hostname
+    maprcli node services -name spark-historyserver -action start -nodes $hostname
+    echo "History Server has been stopped"
+
     break
 elif [ "$choice" == "stop" ]; then
     #$path/stop-history-server.sh
@@ -25,6 +30,8 @@ elif [ "$choice" == "stop" ]; then
     #bash $path/mr-jobhistory-daemon.sh stop historyserver
     #bash $path/yarn-daemon.sh stop historyserver
     maprcli node services -name historyserver -action stop -nodes $hostname
+    maprcli node services -name spark-historyserver -action stop -nodes $hostname
+    echo "History Server has been stopped"
     break
 else
     echo "Invalid choice. Please enter 'start' or 'stop'."
