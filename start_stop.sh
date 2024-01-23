1fd6e53f9625f0cc18e0c2c83b599cfe6a6f0bbc
Haroun RAOUAK <hraouak@outlook.com>
2024-01-23 05:28:49 -0800

    Update

--- start_stop.sh
+++ start_stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #path="/opt/mapr/spark/spark-2.3.1/sbin"
-path = "/opt/mapr/hadoop/hadoop-2.7.0/sbin"
-
+path="/opt/mapr/hadoop/hadoop-2.7.0/sbin"
+hostname="maprdemo"
 
 
 
@@ -15,12 +15,16 @@ read choice
 if [ "$choice" == "start" ]; then
     #$path/start-history-server.sh
     #mapred --daemon start historyserver
-    bash $path/mr-jobhistory-daemon.sh start historyserver
+    #bash $path/mr-jobhistory-daemon.sh start historyserver
+    #bash $path/yarn-daemon.sh start historyserver
+    maprcli node services -name historyserver -action start -nodes hostname
     break
 elif [ "$choice" == "stop" ]; then
     #$path/stop-history-server.sh
     #mapred --daemon stop historyserver
-    bash $path/mr-jobhistory-daemon.sh stop historyserver
+    #bash $path/mr-jobhistory-daemon.sh stop historyserver
+    #bash $path/yarn-daemon.sh stop historyserver
+    maprcli node services -name historyserver -action stop -nodes hostname
     break
 else
     echo "Invalid choice. Please enter 'start' or 'stop'."
