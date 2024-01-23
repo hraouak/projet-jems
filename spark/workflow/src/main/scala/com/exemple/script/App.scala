925a4c805ad637731d6890fc8501d44930d90cd5
Haroun RAOUAK <hraouak@outlook.com>
2024-01-23 10:07:20 +0100

    Update scala file

--- spark/workflow/src/main/scala/com/exemple/script/App.scala
+++ spark/workflow/src/main/scala/com/exemple/script/App.scala
@@ -15,11 +15,11 @@ object App {
 
     val broadcasted1 = functions.broadcast(df1)
     val joined1 = broadcasted1.join(df3, broadcasted1("artist_id") === df3("artist_id"))
-    val selected1 = joined1.select($"full_name".as("artist_name"),$"name".as("work_name"), $"museum_id")
+    val selected1 = joined1.select(col("full_name").as("artist_name"), col("name").as("work_name"), col("museum_id"))
 
     val broadcasted2 = functions.broadcast(df2)
     val joined2 = selected1.join(broadcasted2, selected1("museum_id") === broadcasted2("museum_id"))
-    val result = joined2.select($"artist_name",$"work_name",$"name".as("museum_name"), $"city", $"country")
+    val result = joined2.select(col("artist_name"), col("work_name"), col("name").as("museum_name"), col("city"), col("country"))
 
     // Drop redundant lines
     val cleaned_redundant = result.dropDuplicates()
