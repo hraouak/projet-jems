0b0765f4491586a6ab84c939aba7f139b3e453ef
Haroun RAOUAK <hraouak@outlook.com>
2024-01-23 12:21:03 +0100

    Add start stop script

---
+++ start_stop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+path="/opt/mapr/spark/spark-2.3.1/sbin"
+
+
+
+while true; do
+echo "If you want to start the History Server, type 'start'."
+echo "If you want to stop the History Server, type 'stop'."
+echo -n "Your choice: "
+read choice
+
+if [ "$choice" == "start" ]; then
+    #$path/start-history-server.sh
+    mapred --daemon start historyserver
+    break
+elif [ "$choice" == "stop" ]; then
+    #$path/stop-history-server.sh
+    mapred --daemon stop historyserver
+    break
+else
+    echo "Invalid choice. Please enter 'start' or 'stop'."
+fi
+done
