4c9b653695eb66b3d442dcd76a898e3cbadcfa01
Haroun RAOUAK <hraouak@outlook.com>
2024-01-23 15:28:32 +0100

    Update

--- start_stop.sh
+++ start_stop.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
 
-#path="/opt/mapr/spark/spark-2.3.1/sbin"
-#path="/opt/mapr/hadoop/hadoop-2.7.0/sbin"
 hostname="maprdemo"
 
-
-#echo 'list of services :'
-#echo -e "\n"
-#maprcli service list | awk '{print $2, $5}' | column -t
-#echo -e "\n"
-
-
 while true; do
-echo "If you want to start the History Server, type 'start'."
-echo "If you want to stop the History Server, type 'stop'."
-echo -n "Your choice: "
-read choice
+    choice=$(dialog --clear --backtitle "Interactive Menu" --title "Select Action" \
+                    --menu "Choose an action:" 15 40 4 \
+                    1 "Start History Server" \
+                    2 "Stop History Server" \
+                    3 "Exit" 3>&1 1>&2 2>&3)
 
-if [ "$choice" == "start" ]; then
-    #$path/start-history-server.sh
-    #mapred --daemon start historyserver
-    #bash $path/mr-jobhistory-daemon.sh start historyserver
-    #bash $path/yarn-daemon.sh start historyserver
-    maprcli node services -name historyserver -action start -nodes $hostname
-    maprcli node services -name spark-historyserver -action start -nodes $hostname
-    echo "History Server has been stopped"
-
-    break
-elif [ "$choice" == "stop" ]; then
-    #$path/stop-history-server.sh
-    #mapred --daemon stop historyserver
-    #bash $path/mr-jobhistory-daemon.sh stop historyserver
-    #bash $path/yarn-daemon.sh stop historyserver
-    maprcli node services -name historyserver -action stop -nodes $hostname
-    maprcli node services -name spark-historyserver -action stop -nodes $hostname
-    echo "History Server has been stopped"
-    break
-else
-    echo "Invalid choice. Please enter 'start' or 'stop'."
-fi
+    case $choice in
+        1)
+            maprcli node services -name historyserver -action start -nodes $hostname
+            maprcli node services -name spark-historyserver -action start -nodes $hostname
+            dialog --infobox "History Server started." 5 30
+            sleep 2
+            ;;
+        2)
+            maprcli node services -name historyserver -action stop -nodes $hostname
+            maprcli node services -name spark-historyserver -action stop -nodes $hostname
+            dialog --infobox "History Server stopped." 5 30
+            sleep 2
+            ;;
+        3)
+            echo "Exiting..."
+            exit 0
+            ;;
+    esac
 done
