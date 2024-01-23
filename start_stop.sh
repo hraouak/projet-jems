963153ba0c52af20a3ce5ac142aee99eb6427101
Haroun RAOUAK <hraouak@outlook.com>
2024-01-23 16:13:26 +0100

    Update

--- start_stop.sh
+++ start_stop.sh
@@ -1,40 +1,76 @@
 #!/bin/bash
 
 hostname="maprdemo"
+oozie="/opt/mapr/oozie/oozie-4.3.0/bin/oozie"
+oozie_link="http://localhost:11000/oozie"
+config_path="hive/hive_job.properties"
+spark_submit="/opt/mapr/spark/spark-2.3.1/bin/spark-submit"
+class_name="com.exemple.script.App"
+jar_path="spark/workflow/target/project-1.0-SNAPSHOT.jar"
+
 
 while true; do
     choice=$(dialog --clear --backtitle "Menu" --title "Select Action" \
-                    --menu "Choose an action:" 15 40 4 \
-                    1 "Start Job History Server" \
-                    2 "Stop Job History Server" \
-                    3 "Start Spark History Server" \
-                    4 "Stop Spark History Server "\
-                    5 "Exit" 3>&1 1>&2 2>&3)
+                    --menu "Choose an action:" 50 50 4 \
+                    0 "Job Oozie" \
+                    1 "Job Spark" \ 
+                    2 "Start Job History Server" \
+                    3 "Stop Job History Server" \
+                    4 "Start Spark History Server" \
+                    5 "Stop Spark History Server "\
+                    6 "Start all History Servers "\
+                    7 "Stop all History Servers "\
+                    8 "Exit")
 
     case $choice in
+        0)
+            $oozie job -oozie $oozie_link -config $config_path -run
+            dialog --infobox "Job Oozie executed." 5 50
+            sleep 3
+            ;;
         1)
+            $spark_submit --class $class_name $jar_path
+            dialog --infobox "Job Spark executed." 5 50
+            sleep 3
+            ;;
+
+        2)
             maprcli node services -name historyserver -action start -nodes $hostname
             dialog --infobox "Job History Server started." 5 50
-            sleep 2
+            sleep 3
             ;;
-        2)
+        3)
             maprcli node services -name historyserver -action stop -nodes $hostname
             dialog --infobox "Job History Server stopped." 5 50
-            sleep 2
+            sleep 3
             ;;
 
-        3)
+        4)
             maprcli node services -name spark-historyserver -action start -nodes $hostname
             dialog --infobox "Spark History Server started." 5 50
-            sleep 2
+            sleep 3
             ;;        
 
-        4)
+        5)
             maprcli node services -name spark-historyserver -action stop -nodes $hostname
             dialog --infobox "Spark History Server stopped." 5 50
-            sleep 2
+            sleep 3
+            ;;
+        6)
+            maprcli node services -name historyserver -action start -nodes $hostname
+            maprcli node services -name spark-historyserver -action start -nodes $hostname
+            dialog --infobox "All History Servers are started." 5 50
+            sleep 3
             ;; 
-        5)
+
+
+        7)
+            maprcli node services -name historyserver -action stop -nodes $hostname
+            maprcli node services -name spark-historyserver -action stop -nodes $hostname
+            dialog --infobox "All History Servers are stopped." 5 50
+            sleep 3
+            ;; 
+        8)
             echo "Exiting..."
             exit 0
             ;;
