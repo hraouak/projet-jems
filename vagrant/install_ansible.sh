e692910aa06574f91660cb5c96fb6111f8ee2e60
Haroun RAOUAK <hraouak@outlook.com>
2024-01-24 16:44:16 +0100

    Update ansible installer

--- vagrant/install_ansible.sh
+++ vagrant/install_ansible.sh
@@ -1,1 +1,3 @@
+sudo yum-config-manager --save --setopt=MapR_Core.skip_if_unavailable=true
+sudo yum-config-manager --save --setopt=MapR_Ecosystem.skip_if_unavailable=true
 sudo yum --disablerepo=MapR_Installer --disablerepo=MapR_Ecosystem install ansible -y
